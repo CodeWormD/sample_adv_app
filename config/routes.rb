8fe9e821b846103603f361a16ef28071856c2433
CodeWormD <dan.dad@bk.ru>
2023-11-14 11:36:22 +0300

    Chapter 5 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help' #help_path and help_url
+  get '/about', to: 'static_pages#about' #about_path and about_url
+  get '/contact', to: 'static_pages#contact'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  #users
+  get '/signup', to: 'users#new'
 end
+
